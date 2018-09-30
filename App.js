208d99b4ee0842ddcfd8da3d6ca82ec46126f1ec
Vikas Kumar <vkumar4@paypal.com>
2018-09-30 12:32:53 +0800

    writing exercise screen view

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import MainNavigator from './navigator/MainNavigator';
-import { View } from 'react-native';
-import Sample from './Sample';
 
 export default class App extends React.Component {
   render() {
--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -1,12 +1,92 @@
 import React from 'react';
-import {Text, View } from 'react-native';
+import { Alert, Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 
 export default class ExerciseScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Exercise Screen!</Text>
+  _onPressButton() {
+    Alert.alert("hi");
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.row} >
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Chest</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Back</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Shoulder</Text>
+            </View>
+          </TouchableHighlight>
         </View>
-      );
-    }
+        <View style={styles.row} >
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Biceps</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Abs</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Triceps</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.row} >
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Legs</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Forearms</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.cell} onPress={this._onPressButton}>
+            <View>
+              <Text>Cardio</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 1,
+  },
+  cell: {
+    flex: 1,
+    backgroundColor: 'orange',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight:1,
+  },
+  cellWhite: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  cellGreen: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+
+});
